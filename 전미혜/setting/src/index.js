8befc980074c026e8d171e56714c14c990098535
jeon mihye <mihye2458@gmail.com>
2020-08-18 15:00:12 +0900

    dd

---
+++ 전미혜/setting/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './routers/Main';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+//리액드돔이 렌더링한다, 루트라는 id를 가진 엘리먼트에 h1을 넣어라.
+
---
+++ 전미혜/setting/src/routers/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Main() {
+    return(
+        <h1>여기는 Main Page입니다.</h1> //무조건 큰 껍데기 하나가 감싸야함.
+        //Class 대신 className을 쓴다.
+    );
+};
+
+export default Main;
