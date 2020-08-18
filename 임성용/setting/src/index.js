19b173b0f2d7b8add267fe5cccac262d1e0e4cf9
gold9ine <gold9ine@imseong-yong-ui-MacBook-Pro.local>
2020-08-18 16:30:07 +0900

    200818 LSY first setup

---
+++ 임성용/setting/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './routes/Home';
+import Main from './routes/Main';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
---
+++ 임성용/setting/src/routes/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+	return (
+		<div>
+			<h1>여기는 home 화면 입니다.</h1>
+		</div>
+	);
+};
+
+export default Home;
