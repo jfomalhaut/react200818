e3efaf6e0ba4c7a7c8637a44e46b1e2c53122331
kucu-try <baekfive@naver.com>
2020-08-20 15:03:02 +0900

    dd

---
+++ 백종환/setting/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './routers/Home';
+import Main from './routers/Main';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
---
+++ 백종환/setting/src/routers/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Main() {
+    return(
+        <h1>여기는 Main Page 입니다</h1> // 큰 껍데기 하나 있어야함 무조건!
+    );
+};
+
+export default Main;
