3cef359403b5d62fcf533441f307b8a737c25e4a
lupigomu0520 <lupigomu0520@gmail.com>
2020-08-26 10:50:49 +0900

    dd

---
+++ 이동주/category/src/routers/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const List = () => {
+    return(
+        <div className="container">
+            {'fruit'}
+        </div>
+    );
+};
+
+export default List;
---
+++ 이동주/category/webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    entry: [ //시작점 
+        "./src/index.js" // 이러한 폴더를 만듬 
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.(css)$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: { // 어떻게 출력할지 
+        path: __dirname + "/dist",
+        publicPath: "/",
+        filename: "bundle.js"
+    },
+    devServer: { // 수정한 후 새로고침 없이 바로 수정됨
+        contentBase: "./dist",
+        port: 3000,
+        historyApiFallback: true
+    }
+}    
