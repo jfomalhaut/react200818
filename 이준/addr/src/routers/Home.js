0a0059fc92348b4bb0ed501c57a5a3962c80f77d
이준 <dlwnssla@jfomalhaut.com>
2020-08-27 14:43:11 +0900

    dd

--- 이준/addr/src/routers/Home.js
+++ 이준/addr/src/routers/Home.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import './Home.css';
+
 const JUSO_APIKEY = process.env.JUSO_APIKEY;
 const REQUEST_URL = "http://www.juso.go.kr/addrlink/addrLinkApi.do";
+const VIEW = 10;
 
 const Home = () => {
-	console.log(JUSO_APIKEY);
+	const [keyword, setKeyword] = useState("");
+	const [total, setTotal] = useState(0);
+	const [jusoList, setJusoList] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const onChangeKeyword = (ev) => {
+		const { target: { value } } = ev;
+		setKeyword(value);
+	};
+
+	const search = () => {
+		if (keyword === '') {
+			alert("insert keyword...");
+			return;
+		}
+		onSearch();
+	};
+
+	const onSearch = () => {
+		Axios.get(`${REQUEST_URL}?confmKey=${JUSO_APIKEY}&currentPage=${currentPage}&countPerPage=${VIEW}&resultType=json&keyword=${keyword}`).then(res => {
+			const { data: { results: { common: { totalCount }, juso } } } = res;
+			setTotal(totalCount);
+			setJusoList(juso);
+			// if (totalCount !== '0') {
+			// 	setKeyword('');
+			// }
+		});
+	}
+
+	const onKeyPressKeyword = ev => {
+		if (ev.key === 'Enter') {
+			search();
+		}
+	};
+
+	const prev = () => {};
+
+	const next = () => {
+		setCurrentPage(currentPage + 1);
+	};
+
+	useEffect(() => {
+		if (keyword !== '') {
+			onSearch();
+		}
+	}, [currentPage]);
+
 	return (
-		<h1>Home Component</h1>
+		<div className="container">
+			<input value={keyword} onChange={onChangeKeyword} onKeyPress={onKeyPressKeyword} />
+			<button onClick={search}>검색</button>
+			<h1>keyword value: {keyword}</h1>
+			<h1 style={{ color: 'red'}}>TOTAL: {total}개</h1>
+			<div className="jusoList">
+				{jusoList.map((item, index) => (
+					<div className="item" key={`JUSO${index}`}>
+						<div className="zipNo">{item.zipNo}</div>
+						<div className="juso">
+							<div className="road">{item.roadAddrPart1}</div>
+							<div className="jibun">{item.jibunAddr}</div>
+						</div>
+					</div>
+				))}
+			</div>
+			<button onClick={prev}>이전</button>
+			<button onClick={next}>다음</button>
+		</div>
 	);
 };
 
