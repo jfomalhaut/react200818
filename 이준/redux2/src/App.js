9701a317efd96a603e118cdcdcf28ff98a9b40fb
이준 <dlwnssla@jfomalhaut.com>
2020-09-09 14:53:29 +0900

    dd

--- 이준/redux2/src/App.js
+++ 이준/redux2/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Product, Cart, Detail } from './routers';
+import './Product.css';
 
 const App = () => {
 	return (
--- 이준/redux2/src/routers/Product.js
+++ 이준/redux2/src/routers/Product.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import LIST from '../jsons/fishes.json';
 
 const Product = () => {
+	const [list, setList] = useState([]);
+
+	useEffect(() => {
+		setList(LIST);
+	}, []);
+
 	return (
-		<h1>Product Component</h1>
+		<div className="container">
+			<div className="options">
+				<button>전체선택</button>
+				<button>선택삭제</button>
+				<button>전체삭제</button>
+			</div>
+			<div className="list">
+				{list.map(item => (
+					<div className="item">
+						<div className="img">
+							<img src={item.src} />
+						</div>
+						<div className="info">
+							<div className="name">{item.name}</div>
+							<div className="price">{item.price}원</div>
+							<button>삭제</button>
+							<button>담기</button>
+						</div>
+						<div className="checkbox">
+							<span className="">
+								{/* <GorCheck /> */}
+							</span>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
 	);
 };
 
